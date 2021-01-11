982f8f61dab1b8479e05cb889ff7f884f8fa5601
Windfarer <windfarer@gmail.com>
2021-01-11 12:07:18 +0800

    init ent

---
+++ blog/internal/service/comment.go
@@ -0,0 +1,32 @@
+
+package service
+
+import(
+	"context"
+
+	pb "blog/api/blog/v1"
+)
+
+type CommentService struct {
+	pb.UnimplementedCommentServer
+}
+
+func NewCommentService() pb.CommentServer {
+	return &CommentService{}
+}
+
+func (s *CommentService) CreateComment(ctx context.Context, req *pb.CreateCommentRequest) (*pb.CreateCommentReply, error) {
+	return &pb.CreateCommentReply{}, nil
+}
+func (s *CommentService) UpdateComment(ctx context.Context, req *pb.UpdateCommentRequest) (*pb.UpdateCommentReply, error) {
+	return &pb.UpdateCommentReply{}, nil
+}
+func (s *CommentService) DeleteComment(ctx context.Context, req *pb.DeleteCommentRequest) (*pb.DeleteCommentReply, error) {
+	return &pb.DeleteCommentReply{}, nil
+}
+func (s *CommentService) GetComment(ctx context.Context, req *pb.GetCommentRequest) (*pb.GetCommentReply, error) {
+	return &pb.GetCommentReply{}, nil
+}
+func (s *CommentService) ListComment(ctx context.Context, req *pb.ListCommentRequest) (*pb.ListCommentReply, error) {
+	return &pb.ListCommentReply{}, nil
+}
---
+++ blog/internal/service/post.go
@@ -0,0 +1,33 @@
+
+package service
+
+import(
+	"context"
+
+	pb "blog/api/blog/v1"
+)
+
+type PostService struct {
+	pb.UnimplementedPostServer
+}
+
+func NewPostService() pb.PostServer {
+	return &PostService{}
+}
+
+func (s *PostService) CreatePost(ctx context.Context, req *pb.CreatePostRequest) (*pb.CreatePostReply, error) {
+	return &pb.CreatePostReply{}, nil
+}
+func (s *PostService) UpdatePost(ctx context.Context, req *pb.UpdatePostRequest) (*pb.UpdatePostReply, error) {
+	return &pb.UpdatePostReply{}, nil
+}
+func (s *PostService) DeletePost(ctx context.Context, req *pb.DeletePostRequest) (*pb.DeletePostReply, error) {
+	return &pb.DeletePostReply{}, nil
+}
+func (s *PostService) GetPost(ctx context.Context, req *pb.GetPostRequest) (*pb.GetPostReply, error) {
+	return &pb.GetPostReply{}, nil
+}
+func (s *PostService) ListPost(ctx context.Context, req *pb.ListPostRequest) (*pb.ListPostReply, error) {
+	
+	return &pb.ListPostReply{}, nil
+}
